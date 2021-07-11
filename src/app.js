477678d8ae7a1f8a0ab45ea6e351c925152f2d86
Amit Alon <amit@kaholo.io>
2021-07-12 01:04:20 +0300

    added new method

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-const { getDatabaseClient, getDbWaiter} = require('./helpers');
+const { getDatabaseClient, getDbWaiter, getMySqlSysClient} = require('./helpers');
 const parsers = require('./parsers');
-const {AutonomousDatabase} = require("oci-database").models;
 
 async function createAutonomousDatabase(action, settings){
   const dbClient = getDatabaseClient(settings);
@@ -25,74 +24,41 @@ async function createAutonomousDatabase(action, settings){
   }});
   if (action.params.waitFor){
     const waiters = getDbWaiter(settings);
-    result = await waiters.forAutonomousDatabase(
-      { autonomousDatabaseId: result.autonomousDatabase.id }, // GetAutonomusDatabaseRequest
-      AutonomousDatabase.LifecycleState.Available);
+    result = await waiters.forAutonomousDatabase({ 
+      autonomousDatabaseId: result.autonomousDatabase.id 
+    }, "AVAILABLE");
   }
   return result;
-}
-/* 
-async function createDatabase(action, settings) {
-  const dbClient = getDatabaseClient(settings);
-  return dbClient.createDatabase({
-    compartmentId: parsers.autocomplete(action.params.compartment || settings.tenancyId),
-    createNewDatabaseDetails: {
-      dbHomeId: parsers.autocomplete(action.params.dbHome),
-      dbVersion: parsers.autocomplete(action.params.version)
-    }
-  });
-}
-
-async function createDbHome(action, settings) {
-  const dbClient = getDatabaseClient(settings);
-  return dbClient.createDbHome({
-    compartmentId: parsers.autocomplete(action.params.compartment || settings.tenancyId),
-    createDbHomeWithDbSystemIdDetails: {
-      displayName: parsers.string(action.params.name),
-      dbSystemId: parsers.autocomplete(action.params.dbSystem),
-      dbVersion: parsers.autocomplete(action.params.version)
-    }
-  });
 }
 
 async function createMySqlDatabaseSystem(action, settings) {
-  const mySqlClient = getDbSystemClient(settings);
-  const result = {
-    createDbSystem: await mySqlClient.createDbSystem({
-      createDbSystemDetails: {
-        adminPassword: action.params.adminPassword,
-        adminUsername: parsers.string(action.params.adminUsername),
-        compartmentId: parsers.autocomplete(action.params.compartment || settings.tenancyId),
-        shapeName: parsers.autocomplete(action.params.shape),
-        subnetId: parsers.autocomplete(action.params.subnet),
-        availabilityDomain: parsers.autocomplete(action.params.availabilityDomain),
-        displayName: parsers.string(action.params.name),
-        description: parsers.string(action.params.description),
-        mysqlVersion: parsers.autocomplete(action.params.mysqlVersion),
-        isHighlyAvailable: action.params.isHighlyAvailable || false,
-        port: parsers.number(action.params.port),
-        dataStorageSizeInGBs: parsers.number(action.params.storageSize)
-      }
-    })
-  };
-  if (action.params.createDbHome){
-    if (!action.params.version) throw "Must Specify DB Version for DB Home";
-    action.params.dbSystem = result.createDbSystem.dbSystem.id;
-    const shortId = action.params.dbSystem.substring(action.params.dbSystem.length - 4);
-    action.params.name = `db_home_${shortId}`;
-    result.createDbHome = await createDbHome(action, settings);
-    if (action.params.createDatabase){
-      action.params.dbHome = result.createDbHome.dbHome.id;
-      action.params.name = `db_${shortId}`;
-      result.createDatabase = await createDatabase(action, settings);
-    }
+  const mySqlClient = getMySqlSysClient(settings);
+  let result = await mySqlClient.createDbSystem({ createDbSystemDetails: {
+    adminPassword: action.params.adminPassword,
+    adminUsername: parsers.string(action.params.adminUsername),
+    compartmentId: parsers.autocomplete(action.params.compartment || settings.tenancyId),
+    shapeName: parsers.autocomplete(action.params.shape),
+    subnetId: parsers.autocomplete(action.params.subnet),
+    availabilityDomain: parsers.autocomplete(action.params.availabilityDomain),
+    displayName: parsers.string(action.params.name),
+    description: parsers.string(action.params.description),
+    mysqlVersion: parsers.autocomplete(action.params.mysqlVersion),
+    isHighlyAvailable: action.params.isHighlyAvailable || false,
+    port: parsers.number(action.params.port),
+    dataStorageSizeInGBs: parsers.number(action.params.storageSize)
+  }});
+  if (action.params.waitFor){
+    const waiters = mySqlClient.createWaiters();
+    result = await waiters.forDbSystem({
+      dbSystemId: result.dbSystem.id
+    }, "ACTIVE");
   }
-  else if (action.params.createDatabase) throw "Must Create Default DB Home for default database";
   return result;
-} */
+}
 
 module.exports = {
-  ...require("./autocomplete"),
-  createAutonomousDatabase
+  createAutonomousDatabase,
+  createMySqlDatabaseSystem,
+  ...require("./autocomplete")
 }
 
